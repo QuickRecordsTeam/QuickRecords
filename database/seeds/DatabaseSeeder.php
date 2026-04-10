d990162aa0806105728cbf105df87e3946f1d892
Suh-Edmond <suhedmond25@yahoo.com>
2026-04-10 08:26:42 +0100

    set default seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(RoleAndPermissionSeeder::class);
-         $this->call(\Database\Seeders\SubscriptionPlanSeeder::class);
+        $this->call(RoleAndPermissionSeeder::class);
+        $this->call(\Database\Seeders\SubscriptionPlanSeeder::class);
         // $this->call(OrganisationSeeder::class);
         // $this->call(UserSeeder::class);
         // $this->call(SessionSeeder::class);
